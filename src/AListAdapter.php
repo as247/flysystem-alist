7580d5a3b8dd8f2b19921d06cf64f806ca367329
as247 <as247@vui360.com>
2024-02-03 02:01:51 +0700

    Simplified exception

--- src/AListAdapter.php
+++ src/AListAdapter.php
@@ -2,25 +2,23 @@
 namespace As247\Flysystem\AList;
 use As247\CloudStorages\Storage\AList;
 use As247\Flysystem\DriveSupport\StorageToAdapter;
-use League\Flysystem\Adapter\AbstractAdapter;
+use DateTimeInterface;
+use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\PathPrefixer;
 use League\Flysystem\Config;
+use League\Flysystem\UrlGeneration\TemporaryUrlGenerator;
 
-class AListAdapter extends AbstractAdapter
+class AListAdapter implements FilesystemAdapter, TemporaryUrlGenerator
 {
     use StorageToAdapter;
+    protected $prefixer;
     public function __construct($url, $options = [])
     {
         $this->storage = new AList($url, $options);
-        $this->setPathPrefix($options['root'] ?? '');
+        $this->prefixer = new PathPrefixer($options['root']??'', DIRECTORY_SEPARATOR);
         $this->throwException = $options['debug'] ?? '';
     }
-    public function getTemporaryUrl($path, $expiration=null, Config $config = null){
-        if($expiration===null){
-            $expiration=time()+3600;
-        }
-        if(is_int($expiration)){
-            $expiration=(new \DateTime())->setTimestamp($expiration);
-        }
-        return $this->storage->temporaryUrl($this->applyPathPrefix($path),$expiration,$this->convertConfig($config));
-    }
+
+
+
 }
