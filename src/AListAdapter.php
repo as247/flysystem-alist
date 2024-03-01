0d0e0bc2198fedeb0b86476e74c5d2010988650b
as247 <as247@vui360.com>
2024-03-01 17:47:40 +0700

    Add getTemporaryUrl method

--- src/AListAdapter.php
+++ src/AListAdapter.php
@@ -1,6 +1,7 @@
 <?php
 namespace As247\Flysystem\AList;
 use As247\CloudStorages\Storage\AList;
+use As247\CloudStorages\Support\GetTemporaryUrl;
 use As247\CloudStorages\Support\StorageToAdapter;
 use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\PathPrefixer;
@@ -9,6 +10,7 @@ use League\Flysystem\UrlGeneration\TemporaryUrlGenerator;
 class AListAdapter implements FilesystemAdapter, TemporaryUrlGenerator
 {
     use StorageToAdapter;
+    use GetTemporaryUrl;
     protected $prefixer;
     public function __construct($url, $options = [])
     {
