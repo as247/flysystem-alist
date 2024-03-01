11899de29b437e2fa3471e4f9f5d9edd11692c12
as247 <as247@vui360.com>
2024-03-01 17:34:08 +0700

    Update v1 to match new structure

--- src/AListAdapter.php
+++ src/AListAdapter.php
@@ -1,6 +1,7 @@
 <?php
 namespace As247\Flysystem\AList;
 use As247\CloudStorages\Storage\AList;
+use As247\CloudStorages\Support\GetTemporaryUrl;
 use As247\CloudStorages\Support\StorageToAdapterV1;
 use League\Flysystem\Adapter\AbstractAdapter;
 use League\Flysystem\Config;
@@ -8,19 +9,11 @@ use League\Flysystem\Config;
 class AListAdapter extends AbstractAdapter
 {
     use StorageToAdapterV1;
+    use GetTemporaryUrl;
     public function __construct($url, $options = [])
     {
         $this->storage = new AList($url, $options);
         $this->setPathPrefix($options['root'] ?? '');
         $this->throwException = $options['debug'] ?? '';
-    }
-    public function getTemporaryUrl($path, $expiration=null, Config $config = null){
-        if($expiration===null){
-            $expiration=time()+3600;
-        }
-        if(is_int($expiration)){
-            $expiration=(new \DateTime())->setTimestamp($expiration);
-        }
-        return $this->storage->temporaryUrl($this->applyPathPrefix($path),$expiration,$this->convertConfig($config));
     }
 }
